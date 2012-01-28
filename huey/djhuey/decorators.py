1defd32b622014b544744b52b9c9b51efb53e844
Charles Leifer <coleifer@gmail.com>
2012-01-27 18:18:27 -0600

    decorator with optional parameter can be called in variety of ways

--- huey/djhuey/decorators.py
+++ huey/djhuey/decorators.py
@@ -1,12 +1,15 @@
+import types
+
 from huey.decorators import queue_command as _queue_command, periodic_command as _periodic_command, crontab
 from huey.djhuey import invoker
 
 
 def queue_command(retries=0):
+    if type(retries) == types.FunctionType:
+        return _queue_command(invoker)(retries)
+    
     def inner(fn):
         return _queue_command(invoker, retries)(fn)
-    if type(retries) == type(inner):
-        return inner(retries)
     return inner
 
 def periodic_command(crontab):
