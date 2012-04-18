29b2a8f23c7df6bb4fe8efb68a5af43dc0acf091
Leora <leora.friedrich@chocri.de>
2012-04-18 09:20:11 +0200

    Added possibility to get current retry as parameter

--- huey/djhuey/decorators.py
+++ huey/djhuey/decorators.py
@@ -4,12 +4,12 @@ from huey.decorators import queue_command as _queue_command, periodic_command as
 from huey.djhuey import invoker
 
 
-def queue_command(retries=0, retry_delay=0):
+def queue_command(retries=0, retry_delay=0, retries_as_argument=False):
     if type(retries) == types.FunctionType:
         return _queue_command(invoker)(retries)
     
     def inner(fn):
-        return _queue_command(invoker, retries, retry_delay)(fn)
+        return _queue_command(invoker, retries, retry_delay, retries_as_argument)(fn)
     return inner
 
 def periodic_command(crontab):
