aa3c084645aa400a010e9413301a4034c07bc683
Charles Leifer <coleifer@gmail.com>
2013-05-01 09:07:32 -0500

    Refactoring djhuey

--- huey/djhuey/decorators.py
+++ huey/djhuey/decorators.py
@@ -1,16 +1,15 @@
 import types
 
-from huey.decorators import queue_command as _queue_command, periodic_command as _periodic_command, crontab
-from huey.djhuey import invoker
+from huey.api import crontab
+from huey.djhuey import huey
 
 
-def queue_command(retries=0, retry_delay=0, retries_as_argument=False):
+def task(retries=0, retry_delay=0, retries_as_argument=False):
     if type(retries) == types.FunctionType:
-        return _queue_command(invoker)(retries)
-    
+        return huey.task(retries)
+
     def inner(fn):
-        return _queue_command(invoker, retries, retry_delay, retries_as_argument)(fn)
+        return huey.task(retries, retry_delay, retries_as_argument)(fn)
     return inner
 
-def periodic_command(crontab):
-    return _periodic_command(invoker, crontab)
+periodic_command = huey.periodic_command
