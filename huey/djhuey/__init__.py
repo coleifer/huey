50ecc62341afdaf12e219af4a0d1d8fcb7f1c192
Charles Leifer <coleifer@gmail.com>
2013-05-01 09:07:32 -0500

    Refactoring djhuey

--- huey/djhuey/__init__.py
+++ huey/djhuey/__init__.py
@@ -2,7 +2,7 @@ import sys
 
 from django.conf import settings
 
-from huey.queue import Invoker
+from huey.api import Huey
 from huey.utils import load_class
 
 
@@ -10,82 +10,82 @@ configuration_message = """
 Please configure your queue, example:
 
 HUEY_CONFIG = {
-    'QUEUE': 'huey.backends.redis_backend.RedisQueue',
-    'QUEUE_CONNECTION': 'localhost:6379:0',
-    'THREADS': 4,
+    'queue': 'huey.backends.redis_backend.RedisQueue',
+    'queue_connection': 'localhost:6379:0',
+    'threads': 4,
 }
 
 The following settings are required:
 ------------------------------------
 
-QUEUE (string or Queue instance)
+queue (string or Queue instance)
     Either a queue instance or a string pointing to the module path and class
     name of the queue.  If a string is used, you may also need to specify a
     connection string.
-    
+
     Example: 'huey.backends.redis_backend.RedisQueue'
 
 
 The following settings are recommended:
 ---------------------------------------
 
-QUEUE_NAME (string), default = database name
+queue_name (string), default = database name
 
-QUEUE_CONNECTION (dictionary)
-    If the HUEY_QUEUE was specified using a string, use this parameter to
+queue_connection (dictionary)
+    If the ``queue`` was specified using a string, use this parameter to
     instruct the queue class how to connect.
-    
+
     Example: 'localhost:6379:0' # for the RedisQueue
 
-RESULT_STORE (string or DataStore instance)
+result_store (string or DataStore instance)
     Either a DataStore instance or a string pointing to the module path and
     class name of the result store.
-    
+
     Example: 'huey.backends.redis_backend.RedisDataStore'
 
-RESULT_STORE_NAME (string), default = database name
+result_store_name (string), default = database name
 
-RESULT_STORE_CONNECTION (string)
-    See notes for QUEUE_CONNECTION
-
-TASK_STORE
-    Follows same pattern as RESULT_STORE
+result_store_connection (string)
+    See notes for ``queue_connection``
 
 
 The following settings are optional:
 ------------------------------------
 
-PERIODIC (boolean), default = False
+periodic (boolean), default = False
     Determines whether or not to the consumer will enqueue periodic commands.
     If you are running multiple consumers, only one of them should be configured
     to enqueue periodic commands.
 
-THREADS (int), default = 1
+threads (int), default = 1
     Number of worker threads to use when processing jobs
 
-LOGFILE (string), default = None
+logfile (string), default = None
 
-LOGLEVEL (int), default = logging.INFO
+loglevel (int), default = logging.INFO
 
-BACKOFF (numeric), default = 1.15
+backoff (numeric), default = 1.15
     How much to increase delay when no jobs are present
 
-INITIAL_DELAY (numeric), default = 0.1
+initial_delay (numeric), default = 0.1
     Initial amount of time to sleep when waiting for jobs
 
-MAX_DELAY (numeric), default = 10
+max_delay (numeric), default = 10
     Max amount of time to sleep when waiting for jobs
 
-ALWAYS_EAGER, default = False
+utc (boolean), default = True
+
+always_eager, default = False
     Whether to skip enqueue-ing and run in-band (useful for debugging)
 """
 
-config = getattr(settings, 'HUEY_CONFIG', None)
-if not config or 'QUEUE' not in config:
+_config = getattr(settings, 'HUEY_CONFIG', None)
+config = dict((k.lower(), v) for (k, v) in _config.iteritems())
+if not config or 'queue' not in config:
     print configuration_message
     sys.exit(1)
 
-queue = config['QUEUE']
+queue = config['queue']
 
 if 'default' in settings.DATABASES:
     backup_name = settings.DATABASES['default']['NAME'].rsplit('/', 1)[-1]
@@ -95,29 +95,18 @@ else:
 if isinstance(queue, basestring):
     QueueClass = load_class(queue)
     queue = QueueClass(
-        config.get('QUEUE_NAME', backup_name),
-        **config.get('QUEUE_CONNECTION', {})
+        config.get('queue_name', backup_name),
+        **config.get('queue_connection', {})
     )
-    config['QUEUE'] = queue
 
-result_store = config.get('RESULT_STORE', None)
+result_store = config.get('result_store', None)
 
 if isinstance(result_store, basestring):
     DataStoreClass = load_class(result_store)
     result_store = DataStoreClass(
-        config.get('RESULT_STORE_NAME', backup_name),
-        **config.get('RESULT_STORE_CONNECTION', {})
+        config.get('result_store_name', backup_name),
+        **config.get('result_store_connection', {})
     )
-    config['RESULT_STORE'] = result_store
 
-task_store = config.get('TASK_STORE', None)
-
-if isinstance(task_store, basestring):
-    DataStoreClass = load_class(task_store)
-    task_store = DataStoreClass(
-        config.get('TASK_STORE_NAME', backup_name),
-        **config.get('TASK_STORE_CONNECTION', {})
-    )
-    config['TASK_STORE'] = task_store
-
-invoker = Invoker(queue, result_store, task_store, always_eager=config.get('ALWAYS_EAGER', False))
+always_eager = config.pop('always_eager', False)
+huey = Huey(queue, result_store, always_eager=always_eager)
--- huey/djhuey/management/commands/run_huey.py
+++ huey/djhuey/management/commands/run_huey.py
@@ -6,22 +6,22 @@ from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from django.utils.importlib import import_module
 
-from huey.bin.huey_consumer import Consumer, load_config
-from huey.queue import Invoker
+from huey.bin.huey_consumer import Consumer
+from huey.djhuey import huey, config
 from huey.utils import load_class
 
 
 class Command(BaseCommand):
     """
     Queue consumer.  Example usage::
-    
+
     To start the consumer (note you must export the settings module):
-    
+
     django-admin.py run_huey
     """
-    
+
     help = "Run the queue consumer"
-    
+
     option_list = BaseCommand.option_list + (
         make_option('--periodic', '-p',
             dest='periodic',
@@ -39,12 +39,12 @@ class Command(BaseCommand):
             help='Number of worker threads'
         ),
     )
-    
+
     def autodiscover(self):
-        # this is to find modules named <commands.py> in a django project's
+        # this is to find modules named <tasks.py> in a django project's
         # installed apps directories
-        module_name = 'commands'
-        
+        module_name = 'tasks'
+
         for app in settings.INSTALLED_APPS:
             try:
                 import_module(app)
@@ -57,22 +57,15 @@ class Command(BaseCommand):
                 continue
             import_module('%s.%s' % (app, module_name))
             app_path = sys.modules['%s.%s' % (app, module_name)]
-    
-    def handle(self, *args, **options):
-        config = load_config('huey.djhuey.conf.Configuration')
 
+    def handle(self, *args, **options):
         if options['threads'] is not None:
-            config.THREADS = options['threads']
+            config['threads'] = options['threads']
 
         if options['periodic'] is not None:
-            config.PERIODIC = options['periodic']
-        
+            config['periodic'] = options['periodic']
+
         self.autodiscover()
-        
-        queue = config.QUEUE
-        result_store = config.RESULT_STORE
-        task_store = config.TASK_STORE
-        invoker = Invoker(queue, result_store, task_store)
-        
-        consumer = Consumer(invoker, config)
+
+        consumer = Consumer(huey, **config)
         consumer.run()
