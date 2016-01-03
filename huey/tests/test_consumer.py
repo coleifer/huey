170fcffd11f71967a544604298f244cbfa5a4162
Charles Leifer <coleifer@gmail.com>
2016-01-03 16:45:34 -0600

    Fix test.

--- huey/tests/test_consumer.py
+++ huey/tests/test_consumer.py
@@ -68,7 +68,9 @@ class TestExecution(HueyTestCase):
                 time.sleep(.05)
 
             consumer.stop()
-            consumer.wait_finished()
+            for worker in consumer.worker_threads:
+                worker.join()
+            #consumer.wait_finished()
 
         self.assertEqual(state, {'k1': 'v1', 'k2': 'v2', 'k3': 'v3'})
 
