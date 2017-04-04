f229475c409e49fadfd151ad9f37759a55c0d9ae
Antoine <antoine@buzzhire.co>
2017-04-04 15:04:19 +0100

    Fixed issue when scheduling tasks with timezone-aware ETA

--- huey/utils.py
+++ huey/utils.py
@@ -3,6 +3,40 @@ import sys
 import time
 
 
+class UTC(datetime.tzinfo):
+    """
+    UTC implementation taken from Python's docs.
+    Used only when pytz isn't available.
+    """
+
+    def __repr__(self):
+        return "<UTC>"
+
+    def utcoffset(self, dt):
+        return datetime.timedelta(0)
+
+    def tzname(self, dt):
+        return "UTC"
+
+    def dst(self, dt):
+        return datetime.timedelta(0)
+
+
+def is_naive(dt):
+    """
+    Determines if a given datetime.datetime is naive.
+    The concept is defined in Python's docs:
+    http://docs.python.org/library/datetime.html#datetime.tzinfo
+    Assuming value.tzinfo is either None or a proper datetime.tzinfo,
+    value.utcoffset() implements the appropriate logic.
+    """
+    return dt.utcoffset() is None
+
+
+def is_aware(dt):
+    return not is_naive(dt)
+
+
 def load_class(s):
     path, klass = s.rsplit('.', 1)
     __import__(path)
@@ -15,5 +49,24 @@ def wrap_exception(new_exc_class):
     raise new_exc_class('%s: %s' % (exc_class.__name__, exc))
 
 
+def make_naive(dt):
+    """
+    Makes an aware datetime.datetime naive in its time zone.
+    """
+    return dt.replace(tzinfo=None)
+
+
+def aware_to_utc(dt):
+    """
+    Converts an aware datetime.datetime in UTC time zone.
+    """
+    dt = dt.astimezone(UTC())
+    assert not is_naive(dt), 'Must be a time zone aware datetime'
+    return make_naive(dt)
+
+
 def local_to_utc(dt):
+    """
+    Converts a naive local datetime.datetime in UTC time zone.
+    """
     return datetime.datetime(*time.gmtime(time.mktime(dt.timetuple()))[:6])
