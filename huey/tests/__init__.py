c84caf924a5f4a0ce88656a96dda1f30fb68fef6
Charles Leifer <charles.leifer@propylon.com>
2017-11-02 11:57:43 -0500

    Add tests for hooks.

--- huey/tests/__init__.py
+++ huey/tests/__init__.py
@@ -1,5 +1,6 @@
 from huey.tests.test_consumer import *
 from huey.tests.test_crontab import *
+from huey.tests.test_hooks import *
 from huey.tests.test_queue import *
 from huey.tests.test_registry import *
 from huey.tests.test_storage import *
