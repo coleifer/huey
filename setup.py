ba11ccbcf4b3e91dca9f9bcfc8182d383659fa65
Joe Alcorn <joealcorn123@gmail.com>
2018-01-11 00:18:57 +0000

    Add PyPI python version trove classifiers

--- setup.py
+++ setup.py
@@ -30,6 +30,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='runtests.runtests',
     entry_points={
