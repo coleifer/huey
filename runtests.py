38c53cb82320dca9fa1af780c8f367f42b72dec6
Charles Leifer <coleifer@gmail.com>
2018-05-01 14:48:47 -0500

    Better test runner.

--- runtests.py
+++ runtests.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import optparse
 import os
 import sys
 import unittest
@@ -7,17 +8,33 @@ import unittest
 from huey import tests
 
 
-def runtests(*test_args):
-    suite = unittest.TestLoader().loadTestsFromModule(tests)
-    result = unittest.TextTestRunner(verbosity=1).run(suite)
+def runtests(verbose=False, failfast=False, names=None):
+    if names:
+        suite = unittest.TestLoader().loadTestsFromNames(names, tests)
+    else:
+        suite = unittest.TestLoader().loadTestsFromModule(tests)
+
+    runner = unittest.TextTestRunner(verbosity=2 if verbose else 1,
+                                     failfast=failfast)
+    return runner.run(suite)
+
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_option('-v', '--verbose', action='store_true', default=False,
+                      dest='verbose', help='Verbose output.')
+    parser.add_option('-f', '--failfast', action='store_true', default=False,
+                      help='Stop on first failure or error.')
+    options, args = parser.parse_args()
+    result = runtests(
+        verbose=options.verbose,
+        failfast=options.failfast,
+        names=args)
+
     if os.path.exists('huey.db'):
         os.unlink('huey.db')
+
     if result.failures:
         sys.exit(1)
     elif result.errors:
         sys.exit(2)
-    sys.exit(0)
-
-
-if __name__ == '__main__':
-    runtests(*sys.argv[1:])
