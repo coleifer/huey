43ab16a0696e79fa17604194f156bab1f473742a
Charles Leifer <coleifer@gmail.com>
2019-03-18 21:51:40 -0500

    Consolidate some example code.

--- examples/simple/main.py
+++ examples/simple/main.py
@@ -2,14 +2,19 @@ import os
 if os.environ.get('WORKER_CLASS') in ('greenlet', 'gevent'):
     print('Monkey-patching for gevent.')
     from gevent import monkey; monkey.patch_all()
+import sys
 
 from config import huey
-from tasks import count_beans
+from tasks import add
 
 
 if __name__ == '__main__':
-    try: input = raw_input
-    except NameError: pass
-    beans = input('How many beans? ')
-    count_beans(int(beans))
-    print('Enqueued job to count %s beans' % beans)
+    if sys.version_info[0] == 2:
+        input = raw_input
+
+    print('Huey Demo -- adds two numbers.')
+    a = int(input('a = '))
+    b = int(input('b = '))
+    result = add(a, b)
+    print('Result:')
+    print(result.get(True))
--- examples/simple/tasks.py
+++ examples/simple/tasks.py
@@ -1,34 +1,51 @@
-import random
 import time
 from huey import crontab
 
 from config import huey
 
 
-@huey.task()
-def count_beans(num):
-    print('-- counted %s beans --' % num)
-    return 'Counted %s beans' % num
+def tprint(s, c=32):
+    # Helper to print messages from within tasks using color, to make them
+    # stand out in examples.
+    print('\x1b[1;%sm%s\x1b[0m' % (c, s))
 
-@huey.periodic_task(crontab(minute='*/5'))
-def every_five_mins():
-    print('Consumer prints this every 5 mins')
 
-@huey.task(retries=2, retry_delay=10)
-def try_thrice(msg):
-    print('----TRY THRICE: %s----' % msg)
-    if random.randint(1, 3) == 1:
-        print('OK')
-    else:
-        print('About to fail, will retry in 10 seconds')
-        raise Exception('Uh-oh, something went wrong')
-
-@huey.task()
-def slow(n):
-    time.sleep(n)
-    print('slept %s' % n)
-    return n
+# Tasks used in examples.
 
 @huey.task()
 def add(a, b):
     return a + b
+
+
+@huey.task()
+def mul(a, b):
+    return a * b
+
+
+@huey.task()
+def slow(n):
+    tprint('going to sleep for %s seconds' % n)
+    time.sleep(n)
+    tprint('finished sleeping for %s seconds' % n)
+    return n
+
+
+@huey.task(retries=1, retry_delay=5, context=True)
+def flaky_task(task=None):
+    if task is not None and task.retries == 0:
+        tprint('flaky task succeeded on retry.')
+        return 'succeeded on retry.'
+    tprint('flaky task is about to raise an exception.', 31)
+    raise Exception('flaky task failed!')
+
+
+# Periodic tasks.
+
+@huey.periodic_task(crontab(minute='*/2'))
+def every_other_minute():
+    tprint('This task runs every 2 minutes.', 35)
+
+
+@huey.periodic_task(crontab(minute='*/5'))
+def every_five_mins():
+    tprint('This task runs every 5 minutes.', 34)
