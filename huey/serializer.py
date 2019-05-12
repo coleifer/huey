3c16adeebf2ff18ad8cac4a5e026a4289557e090
Raphael Kimmig <raphael.kimmig@ampad.de>
2019-05-12 11:57:43 +0200

    add signed serializer

--- huey/serializer.py
+++ huey/serializer.py
@@ -1,3 +1,5 @@
+import hashlib
+import hmac
 try:
     import gzip
 except ImportError:
@@ -27,3 +29,58 @@ class Serializer(object):
 
     def deserialize(self, data):
         return self._deserialize(gzip.decompress(data) if self.comp else data)
+
+
+# identical to django.utils.crypto.constant_time_compare
+def _constant_time_compare(val1, val2):
+    """Return True if the two strings are equal, False otherwise."""
+    return hmac.compare_digest(val1, val2)
+
+
+class SignedSerializer(Serializer):
+    def __init__(self, secret="", salt="huey", **kwargs):
+        super().__init__(**kwargs)
+
+        if not secret:
+            raise ConfigurationError("A secret is required")
+
+        if not salt:
+            raise ConfigurationError("A salt is required")
+
+        self.secret = secret.encode("utf-8")
+        self.salt = salt.encode("utf-8")
+
+        # if you change the separator, make sure it doesn't contain
+        # any of [a-zA-Z0-9]
+        self.separator = b":"
+
+    def _signature(self, message: bytes):
+        # same key derivation as django.utils.crypto.salted_hmac
+        key = hashlib.sha1(self.salt + self.secret).digest()
+        signature = hmac.new(key, msg=message, digestmod=hashlib.sha1)
+
+        # we need to make sure that our digest can't contain the separator
+        return signature.hexdigest().encode("utf-8")
+
+    def _sign(self, message: bytes) -> bytes:
+        signature = self._signature(message)
+        return message + self.separator + signature
+
+    def _unsign(self, signed_value: bytes) -> bytes:
+        if self.separator not in signed_value:
+            raise ValueError('Separator "%s" not found in value' % self.separator)
+
+        value, sig = signed_value.rsplit(self.separator, 1)
+
+        if _constant_time_compare(sig, self._signature(value)):
+            return value
+
+        raise ValueError('Signature "%s" does not match' % sig)
+
+    def _serialize(self, data):
+        serialized = super()._serialize(data)
+        return self._sign(serialized)
+
+    def _deserialize(self, data):
+        unsigned = self._unsign(data)
+        return super()._deserialize(unsigned)
