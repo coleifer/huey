9b432d92ad06acaacc29461396fba852fc5dc635
keakon <keakon@gmail.com>
2019-05-16 13:42:03 +0800

    remove unused imports

--- huey/tests/test_serializer.py
+++ huey/tests/test_serializer.py
@@ -1,18 +1,3 @@
-import datetime
-
-from huey.api import MemoryHuey
-from huey.api import PeriodicTask
-from huey.api import Result
-from huey.api import Task
-from huey.api import TaskWrapper
-from huey.api import crontab
-from huey.api import _unsupported
-from huey.constants import EmptyData
-from huey.exceptions import CancelExecution
-from huey.exceptions import ConfigurationError
-from huey.exceptions import RetryTask
-from huey.exceptions import TaskException
-from huey.exceptions import TaskLockedException
 from huey.serializer import Serializer
 from huey.tests.base import BaseTestCase
 
