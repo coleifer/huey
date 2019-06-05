21b008f3c0ffd14ed001122355ee7bc4ba252875
watervize <support@omenapps.com>
2019-06-05 14:43:59 -0700

    Added python versions so django packages shows Python 3 support

--- setup.py
+++ setup.py
@@ -31,6 +31,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='runtests.runtests',
     entry_points={
