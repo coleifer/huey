ef9fbce966e8f365280a6f22c029eb07b8246b70
Charles Leifer <coleifer@gmail.com>
2020-08-13 11:06:42 -0500

    changes suggested to make cons helper script more correct

--- examples/simple/cons.sh
+++ examples/simple/cons.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 echo "HUEY CONSUMER"
 echo "-------------"
 echo "In another terminal, run 'python main.py'"
 echo "Stop the consumer using Ctrl+C"
-PYTHONPATH=.:$PYTHONPATH
-export WORKER_CLASS=${1:-thread}
+PYTHONPATH=".:$PYTHONPATH"
+export PYTHONPATH
+WORKER_CLASS=${1:-thread}
 python ../../huey/bin/huey_consumer.py main.huey --workers=4 -k $WORKER_CLASS -C -S
