86bbf5cb6f924c0acf2d30c8d276ae822f6269ed
Josh <josh@joshthomas.dev>
2021-10-06 08:48:46 -0500

    Update trove classifers

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     test_suite='runtests.collect_tests',
     entry_points={
