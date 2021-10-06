87e5b257078fd82529f33de92c80b128670eb376
Josh <josh@joshthomas.dev>
2021-10-06 08:49:16 -0500

    Add support for Python v3.10

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     test_suite='runtests.collect_tests',
     entry_points={
