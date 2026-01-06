73377c9afdf97ab9f1358e652ae8000445d69b0c
Charles Leifer <coleifer@gmail.com>
2026-01-05 20:54:32 -0600

    New build system

--- setup.py
+++ setup.py
@@ -1,59 +1,6 @@
-import os
 from setuptools import setup, find_packages
-
-
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fh:
-    readme = '\n'.join(line for line in fh.read().splitlines()
-                       if 'image::' not in line)
-
-extras_require = {
-    'backends': ['redis>=3.0.0'],
-    'redis': ['redis>=3.0.0'],
-}
 
 setup(
     name='huey',
-    version=__import__('huey').__version__,
-    description='huey, a little task queue',
-    long_description=readme,
-    author='Charles Leifer',
-    author_email='coleifer@gmail.com',
-    url='http://github.com/coleifer/huey/',
-    project_urls={
-        'Documentation': 'https://huey.readthedocs.io',
-        'Changelog': 'https://github.com/coleifer/huey/blob/master/CHANGELOG.md',
-    },
     packages=find_packages(),
-    extras_require=extras_require,
-    package_data={
-        'huey': [
-        ],
-    },
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    test_suite='runtests.collect_tests',
-    entry_points={
-        'console_scripts': [
-            'huey_consumer = huey.bin.huey_consumer:consumer_main'
-        ]
-    },
-    scripts=['huey/bin/huey_consumer.py'],
-)
+    scripts=['huey/bin/huey_consumer.py'])
