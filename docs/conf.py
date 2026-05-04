50a4a220d04ab987b3447fa4673108f7fa7f89e5
Charles Leifer <coleifer@gmail.com>
2026-05-04 08:32:06 -0500

    Do it this way I guess

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,7 @@ import sys, os
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
