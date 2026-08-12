a188af96a19a41b0db993fc97935534f93972639
糖露星霜•暖霞拾光 <2468001320@qq.com>
2026-08-13 03:38:38 +0800

    fix: replace bare except with except Exception in consumer

--- huey/consumer.py
+++ huey/consumer.py
@@ -410,7 +410,7 @@ class Consumer(object):
                 pass
             except WorkerRecycle:
                 self._logger.info('Process %s restarting (max tasks).', name)
-            except:
+            except Exception:
                 self._logger.exception('Process %s died!', name)
             finally:
                 process.shutdown()
@@ -514,7 +514,7 @@ class Consumer(object):
         except KeyboardInterrupt:
             self._logger.info('Received SIGINT')
             self.stop(graceful=True)
-        except:
+        except Exception:
             self._logger.exception('Error in consumer.')
             self.stop()
         else:
